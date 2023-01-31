235e23847386561ed31c5a86b94128239c090853
Henry <henrysungyun@gmail.com>
2023-01-30 22:57:30 -0800

    another lab done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10) + 1;
+
+let num3 = 44;
+let num4 = 10;
+let mod = num3 % num4;
+
+const max = Math.max(15, 16, 17, 18, 19, 20);
